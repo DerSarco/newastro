07016af2f70bc72b511fe971f700fa6e27dcefb6
erix <eric@webfamous.mx>
2023-04-23 17:01:57 -0600

    app file was moved

---
+++ api/app.py
@@ -0,0 +1,5 @@
+from flask import Flask
+
+from app import create_app
+
+app = create_app()
--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':  
-  app.run(debug=True, port=4000)
