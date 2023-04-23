4618bedd9e4d40a998fdec5c207f18d9b5780c21
erix <eric@webfamous.mx>
2023-04-23 16:15:27 -0600

    app.py was modified

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from app import create_app
 
-if __name__ == '__main__':
-  app = create_app()
+app = create_app()
+
+if __name__ == '__main__':  
   app.run(debug=True, port=4000)
